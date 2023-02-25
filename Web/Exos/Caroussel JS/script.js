bae4a4947b7fde199f831f6550b05e61a7f423d2
EnzoRss <enzo.rossi@ynov.com>
2023-02-25 15:32:36 +0100

    Caroussel JS

---
+++ Web/Exos/Caroussel JS/script.js
@@ -0,0 +1,45 @@
+let container =document.getElementById("container");
+let g =document.getElementById("g")
+let d =document.getElementById("d");
+let p=0;
+let nbr= 5;
+
+function afficherMasquer(){
+if(p ===-nbr+1)
+    d.style.visibility="hidden";
+else
+    d.style.visibility="visible";
+    if(p===0)
+        g.style.visibility="hidden";
+    else
+        g.style.visibility="visible";
+}
+
+document.body.onload =function (){
+
+    container.style.width=(800*nbr)+"px";
+    for (let i=1 ; i<=nbr ;i++){
+        let  div=document.createElement("div")
+        div.className ="photo"
+        div.style.backgroundImage="url('image"+i+".jpg";
+        container.appendChild(div);
+    }
+    afficherMasquer()
+}
+
+d.onclick =function (){
+    if (p>-nbr+1 )
+        p--;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition ="all 0.5s ease"
+    afficherMasquer()
+}
+
+g.onclick =function (){
+    if (p<0 )
+        p++;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition ="all 0.5s ease"
+    afficherMasquer()
+}
+
